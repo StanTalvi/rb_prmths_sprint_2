415c0a5655a7d3bc66646f271cfef4474627f0f1
Stan <ipool22222@gmail.com>
2023-04-27 19:39:15 +0300

    f

---
+++ task_2/Manager.rb
@@ -0,0 +1,14 @@
+require_relative("./Employee")
+
+class Manager < Employee
+    def initialize(first_name, last_name, birthday, salary, bonus)
+        super(first_name, last_name, birthday, salary)
+        @bonus = bonus
+    end
+
+    def salary
+        @salary + @bonus
+    end
+end
+
+
