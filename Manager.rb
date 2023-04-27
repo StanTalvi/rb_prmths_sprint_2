e0a57b16d4d199abf26530c9e8ad16f0b0890832
Stan <ipool22222@gmail.com>
2023-04-27 19:36:47 +0300

    upgrade after Pan Yuriy

--- Manager.rb
+++ Manager.rb
@@ -3,7 +3,11 @@ require_relative("./Employee")
 class Manager < Employee
     def initialize(first_name, last_name, birthday, salary, bonus)
         super(first_name, last_name, birthday, salary)
-        @salary = (@salary + bonus)
+        @bonus = bonus
+    end
+
+    def salary
+        @salary + @bonus
     end
 end
 
